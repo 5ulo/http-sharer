9f00b0ba7d79551563b241d18de893dcc2de59c9
5ulo <peto.5ulo@gmail.com>
2016-01-15 11:15:53 +0100

    Create http-sharer.sh

---
+++ http-sharer.sh
@@ -0,0 +1,18 @@
+#!/bin/sh 
+# date for unicate link
+date=$(date +%Y-%m-%d-%H-%M-%S)
+# public accessible url link
+url="http://your-domain.tld/share/"
+# symlink destination path
+sharepath="/data/http/default/share/"
+# filename + file extension
+file=$(basename $1)
+# create symlink in format date-filename
+ln -s $1 $sharepath$datum"-"$file
+# create full http link for sharing
+link=$url$date"-"$file
+# copy in clipboard
+echo $link | xclip -sel clip
+# output info via zenity dialog or default notification server (eg. libnotify) - comment out what you prefer
+# zenity --info --title="Copied" --text $link
+notify-send -i preferences-system-sharing "Copied" $link
